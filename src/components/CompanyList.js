d123a0715190505334624b807be46ed09e99feaa
tkacperek <tkacperek@gmail.com>
2017-03-27 23:58:31 +0200

    Cleanup.

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -4,11 +4,9 @@ import { Link } from 'react-router'
 import ListView from './ListView'
 
 const itemMapper = key => (
-    // <li className="list-group-item" key={key}>
     <Link className="list-group-item" key={key} to={`/company/${key}`}>
         {key}
     </Link>
-    // </li>
 )
 
 export default () => (
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export default () => (
-    <p>NotFound</p>
+    <div className="container">
+        <h3>NotFound</h3>
+    </div>
 )
